77f7a1142bbe141acd7e74ee5b429cf937692961
renyong <497269678@qq.com>
2019-01-22 15:42:41 +0800

    增加负载均衡算法实现demo

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
@@ -39,6 +39,8 @@ public abstract class AbstractLoadBalance implements LoadBalance {
      * @return weight which takes warmup into account
      */
     static int calculateWarmupWeight(int uptime, int warmup, int weight) {
+        // 计算权重，下面代码逻辑上形似于 (uptime / warmup) * weight。
+        // 随着服务运行时间 uptime 增大，权重计算值 ww 会慢慢接近配置值 weight
         int ww = (int) ((float) uptime / ((float) warmup / (float) weight));
         return ww < 1 ? 1 : (ww > weight ? weight : ww);
     }
@@ -61,6 +63,9 @@ public abstract class AbstractLoadBalance implements LoadBalance {
      * Get the weight of the invoker's invocation which takes warmup time into account
      * if the uptime is within the warmup time, the weight will be reduce proportionally
      *
+     * 该过程主要用于保证当服务运行时长小于服务预热时间时，对服务进行降权，避免让服务在启动之初就处于高负载状态。
+     * 服务预热是一个优化手段，与此类似的还有 JVM 预热。
+     * 主要目的是让服务启动后“低功率”运行一段时间，使其效率慢慢提升至最佳状态
      * @param invoker    the invoker
      * @param invocation the invocation of this invoker
      * @return weight
@@ -68,10 +73,14 @@ public abstract class AbstractLoadBalance implements LoadBalance {
     protected int getWeight(Invoker<?> invoker, Invocation invocation) {
         int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
         if (weight > 0) {
+            // 获取服务提供者启动时间戳
             long timestamp = invoker.getUrl().getParameter(Constants.REMOTE_TIMESTAMP_KEY, 0L);
             if (timestamp > 0L) {
+                // 计算服务提供者运行时长
                 int uptime = (int) (System.currentTimeMillis() - timestamp);
+                // 获取服务预热时间，默认为10分钟
                 int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
+                // 如果服务运行时间小于预热时间，则重新计算服务权重，即降权
                 if (uptime > 0 && uptime < warmup) {
                     weight = calculateWarmupWeight(uptime, warmup, weight);
                 }
